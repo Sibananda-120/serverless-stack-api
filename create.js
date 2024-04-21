e2c0ef9d24bf4074297885b8054aa3b9b8900118
sibananda <sibanandapatra413@gmail.com>
2024-04-21 19:32:13 +0530

    Adding our Serverless API

---
+++ create.js
@@ -0,0 +1,19 @@
+import * as uuid from "uuid"; // Import the uuid library
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName, // Ensure tableName environment variable is defined
+        Item: {
+            userId: "123", // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current Unix timestamp
+        },
+    };
+    await dynamoDb.put(params);
+    return params.Item;
+});
